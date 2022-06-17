4575c285b1a4232746a59b20683e17a75781bcad
caoyaping <caoyapingneu@163.com>
2022-06-17 15:40:09 +0800

    update example_demux_ts.go
    modify code based on mpeg-ts library latest version

--- example/example_demux_ts.go
+++ example/example_demux_ts.go
@@ -1,50 +1,61 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
+    "bytes"
+    "fmt"
+    "io/ioutil"
+    "os"
 
-	"github.com/yapingcat/gomedia/codec"
-	"github.com/yapingcat/gomedia/mpeg2"
+    "github.com/yapingcat/gomedia/codec"
+    "github.com/yapingcat/gomedia/mpeg2"
 )
 
 func main() {
 
     tsfile := os.Args[1]
-    rfd, _ := os.Open(tsfile)
-    buf, _ := ioutil.ReadAll(rfd)
-    fmt.Printf("read %d size\n", len(buf))
-    fd, err := os.OpenFile("1.h265", os.O_CREATE|os.O_RDWR, 0666)
+    tsFd, err := os.Open(tsfile)
     if err != nil {
         fmt.Println(err)
         return
     }
+    defer tsFd.Close()
+    h264FileFd, err := os.OpenFile("video.h264", os.O_CREATE|os.O_RDWR, 0666)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer h264FileFd.Close()
 
-    fd2, err := os.OpenFile("4.aac", os.O_CREATE|os.O_RDWR, 0666)
+    aacFileFd, err := os.OpenFile("audio.aac", os.O_CREATE|os.O_RDWR, 0666)
     if err != nil {
         fmt.Println(err)
         return
     }
-    fd3, err := os.OpenFile("55.log", os.O_CREATE|os.O_RDWR, 0666)
+    defer aacFileFd.Close()
+
+    fd3, err := os.OpenFile("ts_debug.log", os.O_CREATE|os.O_RDWR, 0666)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer fd3.Close()
+
     foundAudio := false
     demuxer := mpeg2.NewTSDemuxer()
     demuxer.OnFrame = func(cid mpeg2.TS_STREAM_TYPE, frame []byte, pts uint64, dts uint64) {
-        if cid == mpeg2.TS_STREAM_H265 {
+        if cid == mpeg2.TS_STREAM_H264 {
             if codec.H265NaluType(frame) == codec.H265_NAL_AUD {
                 return
             }
-            //fmt.Println(len(frame))
-            n, err := fd.Write(frame)
+            n, err := h264FileFd.Write(frame)
             if err != nil || n != len(frame) {
                 fmt.Println(err)
             }
         } else if cid == mpeg2.TS_STREAM_AAC {
             if !foundAudio {
-                fmt.Println("Get AAC")
                 foundAudio = true
             }
-            n, err := fd2.Write(frame)
+            n, err := aacFileFd.Write(frame)
             if err != nil || n != len(frame) {
                 fmt.Println(err)
             }
@@ -85,7 +96,12 @@ func main() {
             }
         }
     }
-    fmt.Println(demuxer.Input(buf))
-    demuxer.Flush()
-    fd.Close()
+
+    buf, _ := ioutil.ReadAll(tsFd)
+    fmt.Printf("read %d size\n", len(buf))
+    fmt.Println(demuxer.Input(bytes.NewReader(buf)))
+    /*
+       if ts file is large,please use bufio.NewReader
+       demuxer.Input(bufio.NewReader(tsFd))
+    */
 }
