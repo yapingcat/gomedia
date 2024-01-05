79f0915d2c19f57fd56862dc5e327928fea80aac
wayyoung <wang1995>
2024-01-06 00:24:35 +0800

    add example_mp4_to_rtmp_server

--- example/example_mp4_to_rtmp_server.go
+++ example/example_mp4_to_rtmp_server.go
@@ -2,27 +2,62 @@ package main
 
 import (
 	"fmt"
-	"github.com/yapingcat/gomedia/go-codec"
-	"github.com/yapingcat/gomedia/go-mp4"
-	"github.com/yapingcat/gomedia/go-rtmp"
 	"io"
 	"net"
 	"os"
 	"time"
+
+	"github.com/yapingcat/gomedia/go-codec"
+	"github.com/yapingcat/gomedia/go-mp4"
+	"github.com/yapingcat/gomedia/go-rtmp"
 )
+
+type TimestampAdjust struct {
+	lastTimeStamp    int64
+	adjust_timestamp int64
+}
+
+func newTimestampAdjust() *TimestampAdjust {
+	return &TimestampAdjust{
+		lastTimeStamp:    -1,
+		adjust_timestamp: 0,
+	}
+}
+
+// timestamp in millisecond
+func (adjust *TimestampAdjust) adjust(timestamp int64) int64 {
+	if adjust.lastTimeStamp == -1 {
+		adjust.adjust_timestamp = timestamp
+		adjust.lastTimeStamp = timestamp
+		return adjust.adjust_timestamp
+	}
+
+	delta := timestamp - adjust.lastTimeStamp
+	if delta < -1000 || delta > 1000 {
+		adjust.adjust_timestamp = adjust.adjust_timestamp + 1
+	} else {
+		adjust.adjust_timestamp = adjust.adjust_timestamp + delta
+	}
+	adjust.lastTimeStamp = timestamp
+	return adjust.adjust_timestamp
+}
+
+var video_pts_adjust *TimestampAdjust = newTimestampAdjust()
+var video_dts_adjust *TimestampAdjust = newTimestampAdjust()
+var audio_ts_adjust *TimestampAdjust = newTimestampAdjust()
 
 // Will push the last file under mp4sPath to the specified rtmp server
 func main() {
 	var (
-		mp4sPath = "your_mp4_dir" //like ./mp4/
-		rtmpUrl  = "rtmpUrl"      //like rtmp://127.0.0.1:1935/live/test110
+		mp4Path = "D:\\soundImage\\bin\\tmp\\initVideo\\video\\"
+		rtmpUrl = "rtmp://127.0.0.1:1935/live/test110"
 	)
 	c, err := net.Dial("tcp4", "127.0.0.1:1935")
 	if err != nil {
 		fmt.Println(err)
 	}
 	cli := rtmp.NewRtmpClient(rtmp.WithComplexHandshake(),
-		rtmp.WithComplexHandshakeSchema(rtmp.HANDSHAKE_COMPLEX_SCHEMA1),
+		rtmp.WithComplexHandshakeSchema(rtmp.HANDSHAKE_COMPLEX_SCHEMA0),
 		rtmp.WithEnablePublish())
 	cli.OnError(func(code, describe string) {
 		fmt.Printf("rtmp code:%s ,describe:%s\n", code, describe)
@@ -43,19 +78,16 @@ func main() {
 	})
 	go func() {
 		<-isReady
-		fmt.Println("start to read mp4 file")
-		ptsbase := uint32(0)
+		fmt.Println("start to read file")
 		for {
-			filees, err := os.ReadDir(mp4sPath)
+			filees, err := os.ReadDir(mp4Path)
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
-
-			fmt.Println(mp4sPath + filees[len(filees)-1].Name())
-			ptsbase = PushRtmp(mp4sPath+filees[len(filees)-1].Name(), cli, ptsbase)
+			fmt.Println(mp4Path + filees[len(filees)-1].Name())
+			PushRtmp(mp4Path+filees[len(filees)-1].Name(), cli)
 		}
-
 	}()
 
 	cli.Start(rtmpUrl)
@@ -66,16 +98,17 @@ func main() {
 		if err != nil {
 			continue
 		}
+		fmt.Println("read byte", n)
 		cli.Input(buf[:n])
 	}
 	fmt.Println(err)
 }
 
-func PushRtmp(fileName string, cli *rtmp.RtmpClient, ptsbase uint32) uint32 {
+func PushRtmp(fileName string, cli *rtmp.RtmpClient) {
 	mp4File, err := os.Open(fileName)
 	if err != nil {
 		fmt.Println(err)
-		return 0
+		return
 	}
 	defer mp4File.Close()
 	demuxer := mp4.CreateMp4Demuxer(mp4File)
@@ -94,15 +127,20 @@ func PushRtmp(fileName string, cli *rtmp.RtmpClient, ptsbase uint32) uint32 {
 			break
 		}
 		if pkg.Cid == mp4.MP4_CODEC_H264 {
-			time.Sleep(10 * time.Millisecond)
-			cli.WriteVideo(codec.CODECID_VIDEO_H264, pkg.Data, uint32(pkg.Pts)+ptsbase, uint32(pkg.Dts))
-			ptsbase = uint32(pkg.Pts) + ptsbase
+			time.Sleep(20 * time.Millisecond)
+			pts := video_pts_adjust.adjust(int64(pkg.Pts))
+			dts := video_dts_adjust.adjust(int64(pkg.Dts))
+			cli.WriteVideo(codec.CODECID_VIDEO_H264, pkg.Data, uint32(pts), uint32(dts))
 		} else if pkg.Cid == mp4.MP4_CODEC_AAC {
-			cli.WriteAudio(codec.CODECID_AUDIO_AAC, pkg.Data, uint32(pkg.Pts), uint32(pkg.Dts))
+			pts := audio_ts_adjust.adjust(int64(pkg.Pts))
+			dts := video_dts_adjust.adjust(int64(pkg.Dts))
+			cli.WriteAudio(codec.CODECID_AUDIO_AAC, pkg.Data, uint32(pts), uint32(dts))
 		} else if pkg.Cid == mp4.MP4_CODEC_MP3 {
-			cli.WriteAudio(codec.CODECID_AUDIO_MP3, pkg.Data, uint32(pkg.Pts), uint32(pkg.Dts))
+			pts := audio_ts_adjust.adjust(int64(pkg.Pts))
+			dts := video_dts_adjust.adjust(int64(pkg.Dts))
+			cli.WriteAudio(codec.CODECID_AUDIO_MP3, pkg.Data, uint32(pts), uint32(dts))
 		}
+
 	}
-	fmt.Println(ptsbase)
-	return ptsbase
+	return
 }
